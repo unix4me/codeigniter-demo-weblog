ac0d88f4637f24541985cec2ef5208b9379dc1a7
unix4me <renejansen@unix4me.nl>
2016-04-26 10:38:12 +0200

    Update highlight.js

--- application/views/theme/header_view.php
+++ application/views/theme/header_view.php
@@ -4,4 +4,4 @@
 
 <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
-<link href="<?php echo base_url(); ?>assets/js/highlight/styles/github.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>assets/css/github.css" rel="stylesheet">
