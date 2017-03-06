ab2d86ddd227d412fa0238cbe4b911547963f8ae
unix4me <rnjnsn62@gmail.com>
2017-03-06 12:46:55 +0100

    Minor update

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <div class="blog-post">
-    <h2 class="blog-post-title"><?php echo $news_item['title']; ?></h2>
+    <h2 class="blog-post-title"><?php echo html_purify($news_item['title']); ?></h2>
     <?php echo html_purify($news_item['text2html']); ?><br />
     <p class="blog-post-meta">
         <?php echo 'Last update on ' , html_purify($news_item['updated']); ?> by <a href="#">User</a>
