04b103c20d254efc648860f4114c8e3a0092e67f
unix4me <rnjnsn62@gmail.com>
2018-03-27 15:17:03 +0200

    Update to version 3.18 and minor changes

--- application/views/theme/template.php
+++ application/views/theme/template.php
@@ -56,7 +56,7 @@
         </div>
         <footer class="blog-footer">
             {footerbar}
-            <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+            <p>If you are exploring <a href="https://codeigniter.com/" target="_blank">CodeIgniter</a> for the very first time, you should start by reading the <a href="/user_guide/">User Guide</a>.</p>
             <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
             <p><a href="#">Back to top</a></p>
         </footer>
