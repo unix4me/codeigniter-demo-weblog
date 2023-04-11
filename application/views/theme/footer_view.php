6211d4be20ebcb9fd8a55d87101d3cc17e11acb2
Unix4Me <rnjnsn62@gmail.com>
2023-04-11 10:12:21 +0200

    a few changes

--- application/views/theme/footer_view.php
+++ application/views/theme/footer_view.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<script src="<?php echo base_url(); ?>js/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>js/jquery-3.6.4.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>js/bootstrap.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>js/highlight.pack.js" type="text/javascript"></script>
 <script>
